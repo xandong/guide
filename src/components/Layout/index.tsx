162602dada5904d0a4aded9a3a367e6285c97797
Alexandre <xandongurgel@gmail.com>
2023-05-21 21:06:04 -0300

    fix import

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./Header";
 import SearchBar from "./SearchBar";
-import Sidebar from "./Sidebar";
 
 interface PageWrapperProps {
   children: React.ReactNode;
@@ -16,7 +15,6 @@ const PageWrapper: React.FC<PageWrapperProps> = (props: PageWrapperProps) => {
         <Header />
 
         <div className="w-full flex-1 flex">
-          {/* <Sidebar /> */}
           <h1 className="hidden">Para onde devo te guiar?</h1>
           <main className="flex-1 py-2 flex flex-col items-center sm:px-10 px-6">
             {
